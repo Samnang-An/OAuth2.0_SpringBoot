4ec9702e80d48df7f59be0fc9d7f2ff3a8338ca6
samnang-an <samnang.an7@gmail.com>
2024-01-30 14:40:06 -0600

    feat: create authorization data OAuth2.0

--- AuthorizationServer/src/main/java/service/AuthenticationServerApplication.java
+++ AuthorizationServer/src/main/java/service/AuthenticationServerApplication.java
@@ -2,7 +2,6 @@ package service;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -18,17 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAuthorizationServer
 public class AuthenticationServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationServerApplication.class, args);
-	}
-	
-	@RequestMapping(value = { "/user" }, produces = "application/json")
-	  public Map<String, Object> user(OAuth2Authentication user) {
-	    Map<String, Object> userInfo = new HashMap<>();
-	    userInfo.put("user", user.getUserAuthentication().getPrincipal());
-	    userInfo.put("authorities",   
-	      AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
-	    return userInfo;
-	    }
+  public static void main(String[] args) {
+    SpringApplication.run(AuthenticationServerApplication.class, args);
+  }
+
+  @RequestMapping(value = {"/user"}, produces = "application/json")
+  public Map<String, Object> user(OAuth2Authentication user) {
+    Map<String, Object> userInfo = new HashMap<>();
+    userInfo.put("user", user.getUserAuthentication().getPrincipal());
+    userInfo.put("authorities",
+        AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+    return userInfo;
+  }
 
 }
--- AuthorizationServer/src/main/java/service/WebSecurityConfigurer.java
+++ AuthorizationServer/src/main/java/service/WebSecurityConfigurer.java
@@ -9,23 +9,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
 
-	@Override
-	@Bean
-	public UserDetailsService userDetailsServiceBean() throws Exception {
-		return super.userDetailsServiceBean();
-	}
+  @Override
+  @Bean
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		  .withUser("john").password("{noop}password1").roles("CUSTOMER")
-		  .and()
-		  .withUser("frank").password("{noop}password2").roles("EMPLOYEE", "MANAGER");
-	}
+  @Override
+  @Bean
+  public UserDetailsService userDetailsServiceBean() throws Exception {
+    return super.userDetailsServiceBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .withUser("john").password("{noop}password1").roles("CUSTOMER")
+        .and()
+        .withUser("frank").password("{noop}password2").roles("CUSTOMER", "EMPLOYEE")
+        .and()
+        .withUser("david").password("{noop}password3").roles("CUSTOMER", "EMPLOYEE", "MANAGER");
+  }
 }
